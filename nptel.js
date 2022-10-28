f7455fbba4a45285298329ec071f953f6c193afc
ashish1677 <116877517+ashish1677@users.noreply.github.com>
2022-10-28 16:09:10 +0530

    nptel.js
    please merge this pull request for me to make a valid contribution towards hacktoberfest

---
+++ nptel.js
@@ -0,0 +1,152 @@
+{
+    {
+        question : "Singrauli can be best categorised as",
+        option : [
+            "mining town",
+            "garrison town",
+            "educational town",
+            "religious town"
+        ],
+        correct : "mining town"
+    },
+    {
+        question : "rain gardens are primarily meant to",
+        option : [
+            "reduce water usage",
+            "increase recharge to groundwater",
+            "protect existing water sources",
+            "increase water flow to streams"
+        ],
+        correct : "increase recharge to groundwater"
+    },
+    {
+        question : "which of these is/ are example(s) of mitigation measures to mitigate impacts of linear infrastructure on wildlife?",
+        option : [
+            "land bridges",
+            "canopy bridges",
+            "glider poles",
+            "all of these"
+        ],
+        correct : "all of these"
+    },
+    {
+        question : "Varanasi can be best categorised as",
+        option : [
+            "mining town",
+            "garrison town",
+            "educational town",
+            "religious town"
+        ],
+        correct : "religious town"
+    },
+    {
+        question : "'the ability to produce a good using fewer inputs than another place' is",
+        option : [
+            "comparative advantage",
+            "absolute advantage",
+            "production advantage",
+            "resource advantage"
+        ],
+        correct : "absolute advantage"
+    },
+    {
+        question : "which of these is a factor governing barrier effect of roads?",
+        option : [
+            "presence and location of animal crossings",
+            "movement pattern of species",
+            "species specific preference of road use",
+            "all of these"
+        ],
+        correct : "all of these"
+    },
+    {
+        question : "Visakhapatnam can be best categorised as",
+        option : [
+            "administrative town",
+            "industrial town",
+            "transport town",
+            "commercial town"
+        ],
+        correct : "transport town"
+    },
+    {
+        question : "which of these is a factor governing barrier effect of roads?",
+        option : [
+            "traffic intensity",
+            "speed of vehicles",
+            "driver sensitivity",
+            "all of these"
+        ],
+        correct : "all of these"
+    },
+    {
+        question : "which of these is/ are example(s) of mitigation measures to mitigate impacts of linear infrastructure on wildlife?",
+        option : [
+            "bridge underpass",
+            "box culverts",
+            "pipe culverts",
+            "all of these"
+        ],
+        correct : "all of these"
+    },
+    {
+        question : "Bhilai can be best categorised as ",
+        option : [
+            "administrative town",
+            "industrial town",
+            "transport town",
+            "commercial town"
+        ],
+        correct : "industrial town"
+    },
+    {
+        question : "which of these is not a use value?",
+        option : [
+            "direct value",
+            "indirect value",
+            "option value",
+            "existence value"
+        ],
+        correct : "existence value"
+    },
+    {
+        question : "'the conditions determined by physical, social, economic and environmental factors or processes which increase the susceptibility of an individual, a community, assets or systems to the impact of hazards.' is a definition for",
+        option : [
+            "hazard",
+            "exposure",
+            "vulnerability",
+            "disaster"
+        ],
+        correct : "vulnerability"
+    },
+    {
+        question : "'a process, phenomenon or human activity that may cause loss of life, injury or other health impacts, property damage, social and economic disruption or environmental degradation.' is a definition for ",
+        option : [
+            "hazard",
+            "exposure",
+            "vulnerability",
+            "disaster"
+        ],
+        correct : "hazard"
+    },
+    {
+        question : "which of these uses imputed willingness to pay?",
+        option : [
+            "market price method",
+            "replacement cost method",
+            "travel cost method",
+            "contingent valuation method"
+        ],
+        correct : "replacement cost method"
+    },
+    {
+        question : "'a serious disruption of the functioning of a community or a society at any scale due to hazardous events interacting with conditions of exposure, vulnerability and capacity, leading to one or more of the following: human, material, economic and environmental losses and impacts' is a definition for",
+        option : [
+            "hazard",
+            "exposure",
+            "vulnerability",
+            "disaster"
+        ],
+        correct : "disaster"
+    }
+}
